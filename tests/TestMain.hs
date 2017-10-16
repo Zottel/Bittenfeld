cf5bb9846841f239e6ddea16979291ea6be93495
Julius Roob <julius@juliusroob.de>
2017-10-16 14:01:26 +0200

    tests: parseSpec renamed to shouldParse.

--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -11,32 +11,32 @@ import Bittenfeld
 parseFS :: Parser a -> ByteString -> Either String a
 parseFS p = parseOnly (p <* endOfInput)
 
-parseSpec p s r = do (parseFS p $ pack s) `shouldBe` (Right r)
+shouldParse p s r = do (parseFS p $ pack s) `shouldBe` (Right r)
 
 main :: IO ()
 main = hspec $ do
   describe "Bittenfeld" $ do
     it "Parses a simple command 1" $ do
-      parseSpec
+      shouldParse
         parse_Command "PING 123"
         (IRC_Command "PING" ["123"])
     it "Parses a simple command 2" $ do
-      parseSpec
+      shouldParse
         parse_Command "PRIVMSG #test :Hallo Welt"
         (IRC_Command "PRIVMSG" ["#test", "Hallo Welt"])
 
     it "Parse a Server" $ do
-      parseSpec
+      shouldParse
         parse_Sender ":irc.z0ttel.net"
         (IRC_Server "irc.z0ttel.net")
 
     it "Parse a User" $ do
-      parseSpec
+      shouldParse
         parse_Sender ":z0ttel!z0ttel@z0ttel.net"
         (IRC_User "z0ttel" "z0ttel" "z0ttel.net")
 
     it "Parse a full line." $ do
-      parseSpec
+      shouldParse
         parse_InMessage ":z0ttel!z0ttel@z0ttel.net PRIVMSG #test :Hallo Welt!"
         (IRC_InMessage (IRC_User "z0ttel" "z0ttel" "z0ttel.net")
                        (IRC_Command "PRIVMSG" ["#test", "Hallo Welt!"]))
